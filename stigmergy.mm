936b464977db3473aad74a1bbed726501582ae00
lilian <contact@lilianricaud.com>
2019-06-28 15:46:09 +0200

    + ‘stigmergy.mm’

---
+++ stigmergy.mm
@@ -0,0 +1,77 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="stigmergy" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1558537611318"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12"/>
+<node TEXT="auto-organisation" POSITION="left" ID="ID_414921484" CREATED="1558537702241" MODIFIED="1558537708901">
+<edge COLOR="#007c00"/>
+<font BOLD="true"/>
+<node TEXT="Accepter les projets parall&#xe8;les (fork/branches)" ID="ID_570568040" CREATED="1558537710015" MODIFIED="1558537710015">
+<node TEXT="accepter et faciliter les branches de projets d&#xe9;velopp&#xe9;s en parall&#xe8;le" ID="ID_735585419" CREATED="1558537769325" MODIFIED="1558537769325"/>
+</node>
+<node TEXT="auto-allocation des taches" ID="ID_1435154056" CREATED="1558537630276" MODIFIED="1558537712793"/>
+<node TEXT="croissance organique et division en noeuds" ID="ID_1418493697" CREATED="1558537621462" MODIFIED="1558537743379"/>
+<node TEXT="Des r&#xe8;gles d&#x2019;interaction simples" ID="ID_18021369" CREATED="1558537776722" MODIFIED="1558537776722"/>
+<node TEXT="Intention commune / Objectifs pr&#xe9;pond&#xe9;rants" ID="ID_199739549" CREATED="1558537785059" MODIFIED="1559028737770"/>
+</node>
+<node TEXT="ouverture a priori" POSITION="right" ID="ID_1499678389" CREATED="1558537717202" MODIFIED="1558537720431">
+<edge COLOR="#7c007c"/>
+<node TEXT="usage de licences libres" ID="ID_392665348" CREATED="1558537655667" MODIFIED="1558537672453"/>
+<node TEXT="large ouverture a priori" ID="ID_960026050" CREATED="1558537669979" MODIFIED="1558537724555"/>
+<node TEXT="grand nombre et grande diversit&#xe9; des agents" ID="ID_424195896" CREATED="1558537638316" MODIFIED="1558537745990"/>
+<node TEXT="les actions doivent laisser des traces perceptibles" ID="ID_499018583" CREATED="1558537687198" MODIFIED="1558537750156"/>
+</node>
+<node TEXT="controle a posteriori" POSITION="right" ID="ID_1175493105" CREATED="1558537729479" MODIFIED="1558537735163">
+<edge COLOR="#007c7c"/>
+<node TEXT="Gestion des taches critiques" ID="ID_1593867438" CREATED="1558537612748" MODIFIED="1558537738013"/>
+<node TEXT="m&#xe9;canisme de contr&#xf4;le a posteriori" ID="ID_900405504" CREATED="1558537649078" MODIFIED="1558537739764"/>
+</node>
+</node>
+</map>
